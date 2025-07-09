3051a155531301cff980a36f37e21d6c40835ddb
Rudra Hatte <rudrarh1806@gmail.com>
2025-07-09 17:02:47 +0530

    Add compiled sql_connection Python bytecode

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -1,8 +1,14 @@
 from sql_connection import get_sql_connection
+import mysql.connector
 
 def get_all_products(connection):
     cursor = connection.cursor()
-    query = ("select products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name from products inner join uom on products.uom_id=uom.uom_id")
+    query = """
+    SELECT products.product_id, products.name, products.uom_id,
+           products.price_per_unit, uom.uom_name
+    FROM products
+    INNER JOIN uom ON products.uom_id = uom.uom_id
+    """
     cursor.execute(query)
     response = []
     for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
@@ -17,29 +23,23 @@ def get_all_products(connection):
 
 def insert_new_product(connection, product):
     cursor = connection.cursor()
-    query = ("INSERT INTO products "
-             "(name, uom_id, price_per_unit)"
-             "VALUES (%s, %s, %s)")
+    query = """
+        INSERT INTO products(name, uom_id, price_per_unit) 
+        VALUES (%s, %s, %s)
+    """
     data = (product['product_name'], product['uom_id'], product['price_per_unit'])
-
     cursor.execute(query, data)
     connection.commit()
-
     return cursor.lastrowid
 
 def delete_product(connection, product_id):
     cursor = connection.cursor()
-    query = ("DELETE FROM products where product_id=" + str(product_id))
-    cursor.execute(query)
+    query = "DELETE FROM products WHERE product_id = %s"
+    cursor.execute(query, (product_id,))
     connection.commit()
-
-    return cursor.lastrowid
+    return cursor.rowcount  # number of rows deleted
 
 if __name__ == '__main__':
     connection = get_sql_connection()
-    # print(get_all_products(connection))
-    print(insert_new_product(connection, {
-        'product_name': 'potatoes',
-        'uom_id': '1',
-        'price_per_unit': 10
-    }))
+    deleted_rows = delete_product(connection, 10)
+    print(f"Deleted rows: {deleted_rows}")
